ae7daa64e94521d758035a7b47d09a4cf8e8dcd3
thaminultazbir <thaminultazbir1999@gmail.com>
2025-10-15 18:45:36 +0600

    view ticket work

--- admin/view_ticket.php
+++ admin/view_ticket.php
@@ -36,7 +36,22 @@ include("./includes/sidenav.php");
     <div class="ticket_details">
       <div class="ticket_heading">
         <h4>Ticket <span>#1 - </span><span>Water Leakage</span></h5>
-        <div class="status">Pending</div>
+        <div class="status pending">Pending</div>
+      </div>
+      <div class="horizontal_line"></div>
+
+      <div class="ticket_body">
+        <div class="project_details">
+          <div><strong class="title">Project: </strong><p>Toruneer</p></div>
+          <div><strong class="title">Floor: </strong><p>4th</p></div>
+          <div><strong class="title">Appartment:</strong><p>B4</p></div>
+        </div>
+        <div><strong>Issue Details: </strong><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, accusantium. Voluptate saepe exercitationem, vitae unde illum modi voluptatum reiciendis impedit commodi. In mollitia alias qui non voluptas quibusdam veritatis illum.</p></div>
+        <div class="project_details">
+          <div><strong class="title">Contact Client: </strong><p>+880123456789</p></div>
+        </div>
+        <div class="project_details"><strong>Assigned Staff: </strong><p>Sazzad</p> <strong>ID: </strong><p>12345</p></div>
+        <div class="project_details"><strong>Change Staff: </strong><p>DropDown</p><button>Update</button></div>
       </div>
     </div>
   </div>
